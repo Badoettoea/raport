ee24539be49d31249a348a3a7c825317d1c74e31
Badoettoea <87426546+Badoettoea@users.noreply.github.com>
2025-04-27 12:59:50 +0700

    Create UserDashboard.vue

---
+++ src/components/UserDashboard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h3>Profil User</h3>
+    <input type="file" @change="uploadFoto" />
+    <img :src="foto" alt="Foto User" v-if="foto" width="100" />
+    <h3>Nilai Raport</h3>
+    <ReportCard :nilai="nilai" />
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import ReportCard from './ReportCard.vue'
+
+const foto = ref('')
+const nilai = ref([
+  { mapel: 'Matematika', nilai: 85 },
+  { mapel: 'Bahasa Indonesia', nilai: 90 }
+])
+
+function uploadFoto(e) {
+  const file = e.target.files[0]
+  if (file) {
+    const reader = new FileReader()
+    reader.onload = (e) => {
+      foto.value = e.target.result
+    }
+    reader.readAsDataURL(file)
+  }
+}
+</script>

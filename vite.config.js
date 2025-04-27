52c2556a95a436a986510f3fa59c25431b02b4b8
Badoettoea <87426546+Badoettoea@users.noreply.github.com>
2025-04-27 13:20:34 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/raport/' // <-- ini wajib! pake nama repo lu!
+  base: '/raport/' // penting kalau deploy ke GitHub Pages
 })

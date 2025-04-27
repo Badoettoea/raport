fbd4b83b28e5d0419511d801d70d4ede3c4cc6a6
Badoettoea <87426546+Badoettoea@users.noreply.github.com>
2025-04-27 13:24:22 +0700

    Update and rename UserView.vue to UserDashboard.vue

---
+++ src/views/UserDashboard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="user-dashboard">
+    <h1>Halo, User!</h1>
+    <p>Ini halaman User Dashboard.</p>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.user-dashboard {
+  padding: 20px;
+}
+</style>

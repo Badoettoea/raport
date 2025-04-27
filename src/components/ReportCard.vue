06618636684ee7175717201e60cf9ad6f07d77d4
Badoettoea <87426546+Badoettoea@users.noreply.github.com>
2025-04-27 13:00:27 +0700

    Create ReportCard.vue

---
+++ src/components/ReportCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div v-for="n in nilai" :key="n.mapel" class="card">
+      <h4>{{ n.mapel }}</h4>
+      <p>Nilai: {{ n.nilai }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps(['nilai'])
+</script>
+
+<style scoped>
+.card {
+  border: 1px solid #ccc;
+  padding: 10px;
+  margin: 5px;
+}
+</style>

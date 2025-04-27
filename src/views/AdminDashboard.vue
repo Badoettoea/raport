dced8ba9f9315c015530df61e9bb57a32027bd95
Badoettoea <87426546+Badoettoea@users.noreply.github.com>
2025-04-27 13:26:11 +0700

    Update and rename AdminView.vue to AdminDashboard.vue

---
+++ src/views/AdminDashboard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="admin-dashboard">
+    <h1>Halo, Admin!</h1>
+    <p>Ini halaman Admin Dashboard.</p>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.admin-dashboard {
+  padding: 20px;
+}
+</style>
